847d8f3b25d753e33898047874841f922abd62e9
aviola98 <viola1@usp.br>
2021-10-20 13:49:42 -0300

    adding notes

--- sp_ethnic_map.R
+++ sp_ethnic_map.R
@@ -6,25 +6,31 @@ library(purrr)
 library(crul)
 library(readxl)
 
+#downloading SP data from geombr
 SP_municipalities <- read_municipality(code_muni = "SP",
                                        year = 2018)
 
+#changing the case of name_muni for future purpouses 
 SP_municipalities <-
 SP_municipalities %>%
   mutate(toupper(name_muni))
   
+#opening candidates database
 candidates <- read_excel("origem.xlsx")
 
+#changing the names in order to do the join operation and filtering the data for SP only
 candidates_SP <- candidates %>%
   filter(UF == "SP") %>%
   rename("toupper(name_muni)" = "CIDADE",
          "abbrev_state" = "UF") 
-  
+ 
+#joining databases 
 sp_ethnic_data <- SP_municipalities %>%
   left_join(candidates_SP,
             c("toupper(name_muni)",
                 "abbrev_state"))
 
+#translating from portuguese to english 
 sp_ethnic_data <-
 sp_ethnic_data %>%
   mutate(origem = case_when(origem == "Ibérico" ~ "Iberian",
@@ -37,9 +43,7 @@ sp_ethnic_data %>%
                             origem == "Francês" ~ "French",
                             TRUE ~ origem))
 
-sp_ethnic_data %>%
-  filter(origem == "Japanese")
-
+#map
 sp_ethnic_data %>%
   st_as_sf(coords=c("geom")) %>%
   ggplot() +
